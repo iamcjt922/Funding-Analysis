e81395e0f5df299c88e9ccf675060e0b02b4cdd8
TSIMEDIACORP <christian@tsimediacorp.com>
2023-06-14 17:01:22 -0700

    Updated with improved scrape algorithm
    Now all 3 bureaus are scraped and their health reported in the output.

--- creditanalysis.py
+++ creditanalysis.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 from PyPDF2 import PdfReader
 import re
 from pdfminer.high_level import extract_pages
-from pdfminer.layout import LTTextContainer
+from pdfminer.layout import LTTextContainer, LTChar
 
 class App:
     def __init__(self, root):
@@ -11,13 +11,11 @@ class App:
         self.root.geometry('300x200')
         self.root.title("Credit Report Scraper")
 
-        # create a button for the upload of the PDF
         self.upload_button = tk.Button(root, text="Upload Credit Report", command=self.upload)
         self.upload_button.pack(pady=20)
 
-        # label that displays the result of the credit report analysis
         self.result_label = tk.Label(root, text="")
-        self.result_label.pack(pady=20)
+        self.result_label.pack(pady=10)
 
     def upload(self):
         filename = filedialog.askopenfilename(initialdir="/", title="Select File",
@@ -28,63 +26,38 @@ class App:
 
     def scrape_pdf(self, filename):
         vantage_score = None
+        transunion_score = None
+        equifax_score = None
+        experian_score = None
+
         for page_layout in extract_pages(filename):
             for element in page_layout:
                 if isinstance(element, LTTextContainer):
-                    for text_line in element:
-                        for character in text_line:
-                            if isinstance(character, LTChar) and character.get_text() == "Vantage Score 3.0":
-                                next_box = text_line.get_bbox().x1 + 1  # Adjust this value as needed
-                                vantage_score = self.extract_score_from_box(page_layout, next_box)
-                                break
+                    text = element.get_text().strip()
+                    if text.startswith("Vantage Score 3.0"):
+                        vantage_score = self.extract_score_from_grid(page_layout, element.index + 1)
+                        transunion_score = self.extract_score_from_grid(page_layout, element.index + 2)
+                        equifax_score = self.extract_score_from_grid(page_layout, element.index + 3)
+                        experian_score = self.extract_score_from_grid(page_layout, element.index + 4)
+                        break
 
         if vantage_score:
-            if self.check_vantage_score(vantage_score) and self.check_late_payments(text):
-                self.result_label['text'] = "This is a great funding candidate!"
-                self.reason_label['text'] = "Reason: High credit score and few late payments."
-            else:
-                self.result_label['text'] = "This candidate will more than likely not be approved"
-                self.reason_label['text'] = "Reason: Low credit score or too many late payments."
+            self.result_label['text'] = f"Vantage Score: {vantage_score}\n" \
+                                         f"Transunion Score: {transunion_score}\n" \
+                                         f"Equifax Score: {equifax_score}\n" \
+                                         f"Experian Score: {experian_score}"
         else:
-            print("Could not find Vantage Score 3.0 in text.")
+            self.result_label['text'] = "Could not find Vantage Score 3.0 in the credit report."
 
-    def extract_score_from_box(self, page_layout, x_max):
+    def extract_score_from_grid(self, page_layout, index):
         score = ""
         for element in page_layout:
-            if isinstance(element, LTTextContainer):
-                for text_line in element:
-                    for character in text_line:
-                        if isinstance(character, LTChar) and character.get_bbox().x0 > x_max:
-                            score += character.get_text()
+            if isinstance(element, LTTextContainer) and element.index == index:
+                score = element.get_text().strip()
+                break
         return score
-
-    @staticmethod
-    def check_credit(text):
-        # Credit Check Algorithm 
-        match = re.search(r"Credit Score: (\d+)", text)
-        if match:
-            return int(match.group(1)) > 700
-        else:
-            return False
-
-    @staticmethod
-    def check_vantage_score(text):
-        match = re.search(r"Vantage Score 3.0:\s*(\d{3})", text)
-        if match:
-            return int(match.group(1)) > 700
-        else:
-            return False
-
-    @staticmethod
-    def check_late_payments(text):
-        # Late payments algorithm
-        return len(re.findall(r"Late Payment", text)) <= 2
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
     root.mainloop()
-
-
-#check if scores are being scraped properly
-print(text)
